d090aac0e7f6a72e7df3a052d9af861064c0106e
adslyw <adslyw@gmail.com>
2024-09-30 17:31:06 +0800

    lens

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
-  if (req.url.startsWith("/v3/upload")){
-    target = "https://lens.google.com";
-  }
+  let target = "https://lens.google.com/";//your website url
 
   createProxyMiddleware({
     target,
